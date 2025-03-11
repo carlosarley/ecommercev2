46ec86530c48a4ee0898e043bf437b6f89e91115
carlosarley <carlosarley0829@gmail.com>
2025-03-10 23:56:02 -0500

    se agrego bg orange a carrito

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -145,7 +145,7 @@ const Header: React.FC = () => {
         {/* Carrito - 5% */}
         <div className="basis-1/24">
           <Link to="/cart" className="flex items-center justify-center">
-            <span className="relative flex items-center justify-center bg-[--button-bg] text-[--button-text] px-2 py-1 rounded-full hover:bg-[--button-bg-hover]">
+            <span className="bg-orange relative flex items-center justify-center bg-[--button-bg] text-[--button-text] px-2 py-1 rounded-full hover:bg-[--button-bg-hover]">
               🛒
               {totalItems > 0 && (
                 <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
